21354805fbbe3a30b67139e4c5118ce77a27105b
Muntakim Unnur Lamim <82176316+lamim-cyber@users.noreply.github.com>
2025-06-10 19:47:43 +0600

    Update and rename 8. Chemistry to 8. Chemistry.cpp

--- 8. Chemistry.cpp
+++ 8. Chemistry.cpp
@@ -1,3 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define hi ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define pb push_back
+#define in(j,n,v) for(int i=j; i<n; i++) cin>> v[i];
+#define out(j,n,v) for(int i=j; i<n; i++) cout<< v[i]<<" ";
+#define mp(x, y) make_pair(x, y)
+#define yes cout << "YES" << '\n';
+#define no cout << "NO" << '\n';
+#define test int t; cin >> t; while (t--)
+#define srt(v) sort(v.begin(), v.end());
+#define rsrt(v) sort(v.rbegin(), v.rend());
+#define rvs(v) reverse(v.begin(), v.end());
+#define F first
+#define S second
+#define ld long double
+using namespace std;
 int main()
 {
    test

2eb7db75250e95e2f3ae15607a40a5c0d77f11e7
Muntakim Unnur Lamim <82176316+lamim-cyber@users.noreply.github.com>
2025-06-10 19:48:34 +0600

    Update and rename 6. Odd Divisor to 6. Odd Divisor.cpp

---
+++ 6. Odd Divisor.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define hi ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define pb push_back
+#define in(j,n,v) for(int i=j; i<n; i++) cin>> v[i];
+#define out(j,n,v) for(int i=j; i<n; i++) cout<< v[i]<<" ";
+#define mp(x, y) make_pair(x, y)
+#define yes cout << "YES" << '\n';
+#define no cout << "NO" << '\n';
+#define test int t; cin >> t; while (t--)
+#define srt(v) sort(v.begin(), v.end());
+#define rsrt(v) sort(v.rbegin(), v.rend());
+#define rvs(v) reverse(v.begin(), v.end());
+#define F first
+#define S second
+#define ld long double
+#define nl '\n'
+#define iint 1000000000
+#define mod 1000000007
+#define mod2 998244353
+using namespace std;
+int main()
+{
+    test
+    {
+        ll n;
+        cin>> n;
+        if(n==2)
+        {
+            no
+            continue;
+        }
+    bool flag=0;
+        while(n>2)
+        {
+            if(n%2==1)
+                {
+ 
+                    flag =1;break;
+                }
+ 
+            else
+            {
+                n/=2;
+            }
+        }
+ 
+        if(flag)
+            yes
+        else
+        no
+    }
+}
